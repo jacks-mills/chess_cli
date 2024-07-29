aa3e7948e35451e0fbb30281aa74bdedb3ae63a8
jacks-mills <millsjack002@gmail.com>
2024-07-29 21:10:31 +1000

    Add prototypes to view c and header files

---
+++ scr/view.c
@@ -0,0 +1,10 @@
+#include "view.h"
+
+
+void view_draw_empty_board(int width, int height) {
+
+}
+
+void view_draw_piece(int rank, int file, int piece) {
+
+}
---
+++ scr/view.h
@@ -0,0 +1,7 @@
+#ifndef _VIEW_H
+#define _VIEW_H
+
+void view_draw_empty_board(int width, int height);
+void view_draw_piece(int rank, int file, int piece);
+
+#endif //_VIEW_H
